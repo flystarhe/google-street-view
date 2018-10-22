6a4de552d715a9aafee6f7654ba5bf9a3cdd3909
flystarhe <flystarhe@qq.com>
2018-10-22 15:05:09 +0800

    update: 2018-10-22 15:05

--- plan.sh
+++ plan.sh
@@ -1,19 +1,5 @@
-# Google Street View
-
-## test
-```
-python streetview.py
-python streetview.py 30.657054,104.065665
-```
-
-## bash
-```commandline
-nohup python geo.py > logs.nohup.geo 2>&1 &
-nohup python tool.py 100 > logs.nohup.tool 2>&1 &
-
 nohup python main.py images_100 > logs.nohup.main 2>&1 &
 nohup python main.py images_100 0 > logs.nohup.main 2>&1 &
 nohup python main.py images_100 90 > logs.nohup.main 2>&1 &
 nohup python main.py images_100 180 > logs.nohup.main 2>&1 &
 nohup python main.py images_100 270 > logs.nohup.main 2>&1 &
-```
