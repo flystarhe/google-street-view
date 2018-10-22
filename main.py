e1ac295938868b06846e565b30503a6c6ff36f74
flystarhe <flystarhe@qq.com>
2018-10-22 12:57:07 +0800

    update: 2018-10-22 12:57

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import json
 import codecs
 from streetview import download2 as download
 
-log_file = "google-street-view/logs.locations.100"
+log_file = "logs.locations.100"
 with codecs.open(log_file, "r", "utf-8") as readers:
     logs = [line for line in readers.readlines() if line.startswith(":")]
     locations = [json.loads(line[1:]) for line in logs]
 print("locations size:", len(locations))
 
-root = sys.argv[1] if len(sys.argv) > 1 else "downloads"
+root = sys.argv[1] if len(sys.argv) > 1 else "images"
 api_key = "AIzaSyCw5exiqqFXVSQoNEdf4M43Jr0LlLcL4zY"
 secret = "Hkk3M1Z8gyEQ17YPwi5iit-ZHI0="
 
--- readme.md
+++ readme.md
@@ -8,6 +8,9 @@ python streetview.py 30.657054,104.065665
 
 ## bash
 ```commandline
-nohup python geocode.py > /data/logs/log.google.00 2>&1 &
-nohup python main.py > /data/logs/log.google.01 2>&1 &
+nohup python geocode.py > log.nohup.geo 2>&1 &
+nohup python tool.py 100 > logs.nohup.tool 2>&1 &
+nohup python main.py images_100 > log.nohup.main 2>&1 &
+nohup python main.py images_100 0 > log.nohup.main 2>&1 &
+nohup python main.py images_100 90 > log.nohup.main 2>&1 &
 ```
