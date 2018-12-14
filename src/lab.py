dc8524149c895001d4f615d87e23dca834302303
flystarhe <flystarhe@qq.com>
2018-12-14 12:10:03 +0800

    update: 2018-12-14 12:10

---
+++ src/lab.py
@@ -0,0 +1,33 @@
+import os
+import json
+import codecs
+
+
+def log_imgs_to_json(log_file, simple=True):
+    names = ["location", "pano_id", "log_id", "image_path"]
+
+    data = []
+    with open(log_file) as log_file:
+        for l in log_file:
+            if not l.startswith(":"):
+                continue
+            l = json.loads(l[1:])
+            data.append([l[i] for i in names])
+
+    if simple:
+        for i in data:
+            i[-1] = os.path.basename(i[-1])
+
+    return data
+
+
+def list_to_file(data, file_name, output_dir="."):
+    output_dir = os.path.realpath(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
+
+    file_path = os.path.join(output_dir, file_name)
+    with codecs.open(file_path, "w", "utf-8") as writer:
+        for line in data:
+            writer.write(json.dumps(line))
+            writer.write("\n")
+    return file_path
---
+++ src/tool.py
@@ -0,0 +1,94 @@
+import os
+import sys
+import time
+import math
+import json
+import codecs
+import functools
+import multiprocessing
+import numpy as np
+from src.streetview import request_metadata
+
+
+class Logger(object):
+    def __init__(self, root, name="log.data"):
+        os.makedirs(root, exist_ok=True)
+        self.path = os.path.join(root, name)
+        self.data = ["-" * 50]
+        self.save("w")
+
+    def log(self, message):
+        self.data.append(message)
+        if len(self.data) > 100:
+            self.save("a")
+
+    def save(self, mode):
+        with codecs.open(self.path, mode, "utf-8") as writer:
+            message = "\n".join(self.data)
+            writer.write(message)
+            writer.write("\n")
+        self.data = []
+
+
+def seq_unique(items, attr_name):
+    res, keys = list(), set()
+    for item in items:
+        key = item[attr_name]
+        if key not in keys:
+            res.append(item)
+            keys.add(key)
+    print("#total: {}, only value: {}".format(len(items), len(res)))
+    return res
+
+
+def get_metadata(api_key, secret, location):
+    res = request_metadata(key=api_key, secret=secret, location=location)
+    res["query_params"] = {"location": location}
+    if res["status"] == "OK":
+        return ":{}".format(json.dumps(res))
+    return "!{}".format(json.dumps(res))
+
+
+def get_locations(lat1, lat2, lng1, lng2, step=1000):
+    locations = []
+    R = 6377.830 * 1000
+    lat_step = 1 / (R * np.pi / 180) * step
+    for lat in np.arange(lat1, lat2, lat_step):
+        lng_step = 1 / (R * np.pi / 180 * math.cos(lat)) * step
+        for lng in np.arange(lng1, lng2, lng_step):
+            locations.append("{:.6f},{:.6f}".format(lat, lng))
+    return locations
+
+
+def get_locations_pano(api_key, secret, locations):
+    func = functools.partial(get_metadata, api_key, secret)
+    with multiprocessing.Pool(processes=16) as pool:
+        result = pool.map(func, locations)
+    return result
+
+
+def save_logs(logs, log_file):
+    with codecs.open(log_file, "w", "utf-8") as writer:
+        counta = len([True for i in logs if i.startswith(":")])
+        countb = len([True for i in logs if i.startswith("!")])
+        countc = len([True for i in logs if i.startswith("?")])
+        writer.write(time.strftime("#%Y-%m-%d %H:%M:%S [{}/{}/{}]\n".format(counta, countb, countc)))
+        writer.write("\n".join(logs))
+        writer.write("\n")
+    return log_file
+
+
+if __name__ == "__main__":
+    api_key = "AIzaSyCw5exiqqFXVSQoNEdf4M43Jr0LlLcL4zY"
+    secret = "Hkk3M1Z8gyEQ17YPwi5iit-ZHI0="
+    lat1 = 30.488286
+    lat2 = 30.7620646
+    lng1 = 103.921107
+    lng2 = 104.2523822
+    step = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    print(time.strftime("#%Y-%m-%d %H:%M:%S"))
+    locations = get_locations(lat1, lat2, lng1, lng2, step)
+    print(time.strftime("#%Y-%m-%d %H:%M:%S"))
+    logs = get_locations_pano(api_key, secret, locations)
+    print(time.strftime("#%Y-%m-%d %H:%M:%S"))
+    print(save_logs(logs, "logs.locations.{}".format(step)), len(logs))
