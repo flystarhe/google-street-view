f4cb77de3dc61eb9111027418e7ac6d1823c5ced
flystarhe <flystarhe@qq.com>
2018-12-17 22:52:36 +0800

    2018-12-17 22:52

--- src/score/__init__.py
+++ src/score/__init__.py
@@ -1,9 +1,10 @@
 import os
 import codecs
 import random
+import torchvision.transforms as transforms
 
 
-def split_dataset(file_path, shuffle=True):
+def split_dataset(file_path, shuffle=True, keep=False):
     data = []
     with open(file_path) as file:
         for line in file:
@@ -14,17 +15,37 @@ def split_dataset(file_path, shuffle=True):
     if shuffle:
         random.shuffle(data)
 
-    root = os.path.dirname(file_path)
-    root = os.path.realpath(root)
-
     pos = int(len(data) * 0.2)
+    print("data size: {}, split pos: {}".format(len(data), pos))
 
-    file_path = os.path.join(root, "dataset_val.txt")
-    with codecs.open(file_path, "w", "utf-8") as writer:
-        writer.write("\n".join(data[:pos]))
+    if keep:
+        root = os.path.dirname(file_path)
+        root = os.path.realpath(root)
 
-    file_path = os.path.join(root, "dataset_train.txt")
-    with codecs.open(file_path, "w", "utf-8") as writer:
-        writer.write("\n".join(data[pos:]))
+        val_file = os.path.join(root, "dataset_val.txt")
+        with codecs.open(val_file, "w", "utf-8") as writer:
+            writer.write("\n".join(data[:pos]))
 
-    return len(data), pos
+        train_file = os.path.join(root, "dataset_train.txt")
+        with codecs.open(train_file, "w", "utf-8") as writer:
+            writer.write("\n".join(data[pos:]))
+
+        return val_file, train_file
+    else:
+        return data[:pos], data[pos:]
+
+
+def get_transform(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], mode="test"):
+    if mode == "train":
+        return transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)])
+    else:
+        return transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)])
