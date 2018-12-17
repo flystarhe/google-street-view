2a61d718a4d52192278bd4d2eb8520b5a679a27d
flystarhe <flystarhe@qq.com>
2018-12-17 10:00:25 +0800

    update: 2018-12-17 09:59

--- src/pulse_format.py
+++ src/pulse_format.py
@@ -3,8 +3,11 @@ import json
 import codecs
 
 
-def log_imgs_to_json(log_file, simple=True):
-    names = ["location", "pano_id", "log_id", "image_path"]
+def log_imgs_to_json(log_file, check=True):
+    root = os.path.dirname(log_file)
+    root = os.path.realpath(root)
+
+    names = ["image_path", "log_id"]
 
     data = []
     with open(log_file) as log_file:
@@ -14,9 +17,9 @@ def log_imgs_to_json(log_file, simple=True):
             l = json.loads(l[1:])
             data.append([l[i] for i in names])
 
-    if simple:
+    if check:
         for i in data:
-            i[-1] = os.path.basename(i[-1])
+            i[0] = os.path.join(root, os.path.basename(i[0]))
 
     return data
 
@@ -28,13 +31,13 @@ def list_to_file(data, file_name, output_dir="."):
     file_path = os.path.join(output_dir, file_name)
     with codecs.open(file_path, "w", "utf-8") as writer:
         for line in data:
-            writer.write(json.dumps(line))
+            writer.write(" ".join(line))
             writer.write("\n")
     return file_path
 
 
 if __name__ == "__main__":
-    log_file = "/data1/tmps/?/log.imgs"
-    data = log_imgs_to_json(log_file, simple=True)
-    res = list_to_file(data, "dataset.txt", os.path.dirname(log_file))
+    log_file = "/data1/tmps/images_pulse/log.imgs"
+    data = log_imgs_to_json(log_file, check=True)
+    res = list_to_file(data, "dataset.txt", "tmps")
     print(res)
