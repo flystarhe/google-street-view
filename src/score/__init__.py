feb75b4f2b8481298a5e19510c84169e8ea53e04
flystarhe <flystarhe@qq.com>
2018-12-17 23:04:25 +0800

    2018-12-17 23:04

--- src/score/__init__.py
+++ src/score/__init__.py
@@ -39,7 +39,7 @@ def get_transform(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], mode="t
     if mode == "train":
         return transforms.Compose([
             transforms.Resize(256),
-            transforms.RandomResizedCrop(224),
+            transforms.RandomCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean, std)])
