5f92a3750bd0a3f67ae0ca7dbef1d5315255fc90
flystarhe <flystarhe@qq.com>
2018-12-18 15:43:27 +0800

    update: 2018-12-18 15:42

---
+++ src/app/__init__.py
@@ -0,0 +1,20 @@
+import os
+from src.score.regress import load_gmm, load_svr
+from src.score.regress import get_fisher_vectors
+
+base_dir = "/home/hejian/PycharmProjects/google-street-view/models"
+
+gmm_path = "gmm"
+svr_path = {'beautiful': 'svr.model.12181152', 'boring': 'svr.model.12181231', 'depressing': 'svr.model.12181251',
+            'lively': 'svr.model.12181330', 'safety': 'svr.model.12181410', 'wealthy': 'svr.model.12181432'}
+
+gmm = load_gmm(os.path.join(base_dir, gmm_path))
+svr_list = {name: load_svr(os.path.join(base_dir, model_path)) for name, model_path in svr_path.items()}
+
+
+def inference(image_list):
+    # image_list: [image_file_path,..]
+    global gmm, svr_list
+    X, y = get_fisher_vectors([[image, None] for image in image_list], None, gmm)
+    rs = {name: svr.predict(X) for name, svr in svr_list.items()}
+    return rs
