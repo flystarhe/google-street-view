bc6cb2b732e6c594e0b71568cf030e93e70d0519
flystarhe <flystarhe@qq.com>
2018-12-20 10:32:20 +0800

    update: app/*

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,20 +1,41 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from src.dnn.model import Net
+
+"""
 import os
-from src.score.regress import load_gmm, load_svr
-from src.score.regress import get_fisher_vectors
+import sys
 
-base_dir = "/home/hejian/PycharmProjects/google-street-view/models"
+mylibs = ["/home/hejian/PycharmProjects/google-street-view"]
+os.chdir(mylibs[0])
+for mylib in mylibs:
+    if mylib not in sys.path:
+        sys.path.insert(0, mylib)
+"""
 
-gmm_path = "gmm"
-svr_path = {'beautiful': 'svr.model.12181152', 'boring': 'svr.model.12181231', 'depressing': 'svr.model.12181251',
-            'lively': 'svr.model.12181330', 'safety': 'svr.model.12181410', 'wealthy': 'svr.model.12181432'}
-
-gmm = load_gmm(os.path.join(base_dir, gmm_path))
-svr_list = {name: load_svr(os.path.join(base_dir, model_path)) for name, model_path in svr_path.items()}
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
-def inference(image_list):
-    # image_list: [image_file_path,..]
-    global gmm, svr_list
-    X, y = get_fisher_vectors([[image, None] for image in image_list], None, gmm)
-    rs = {name: svr.predict(X).clip(0, 10) for name, svr in svr_list.items()}
-    return rs
+def load_net(model_path, device):
+    net = Net(num_classes=6, pretrained=False).to(device)
+    net.load_state_dict(torch.load(model_path, map_location=device))
+    net.to(device)
+    net.eval()
+    return net
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_path = "/data1/tmps/street_view/net_00000500.pth"
+model = load_net(model_path, device)
+trans = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(256), transforms.ToTensor(), normalize])
+
+
+def inference(image_path, device):
+    # image_path = "/data1/tmps/images_pulse/-x46IJ2AQTU4FWu9WiCRfw,1214011640.jpg"
+    image = Image.open(image_path)
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    x = trans(image).unsqueeze(0).to(device)
+    y = torch.sigmoid(model(x))
+    return y.tolist()
